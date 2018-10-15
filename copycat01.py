eac58e0afb6a97b88ca6b30c5bb62361d32f0c9c
Daniel Adams <daniel.adams@scotiabank.com>
2018-10-15 16:08:57 -0400

    Lab 8 commit

---
+++ copycat01.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+import shutil
+import os
+os.chdir('/home/student/mycode/')
+shutil.copy('5g_research/sdn_network.txt', '5g_research/sdn_network.txt.copy')
+shutil.copytree('5g_research/', '5g_research_backup/')
+
+
