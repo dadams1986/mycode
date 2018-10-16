229266845068f685ccda5634de8e323e3f7b21bd
Daniel Adams <daniel.adams@scotiabank.com>
2018-10-16 15:24:21 -0400

    lab20

---
+++ introfor/forloop1.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+vendors = ['cisco', 'juniper','big_ip', 'f5', 'arista']
+approved_vendors = ['cisco', 'juniper', 'big_ip']
+
+for x in vendors:
+  print("\nThe vendor is:" +x, end="")
+  if x not in approved_vendors:
+    print(" - NOT AN APPROVED VENDOR!", end="")
+print("\nOur loop has ended.")
+
+
